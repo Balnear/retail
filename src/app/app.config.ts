25633613af0cce6fe9fd9148ca038dd61d4b63c9
Balnear <antoniomelchionda70@gmail.com>
2024-09-29 19:11:08 +0200

    fix-code

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,13 +10,11 @@ import { AngularMaterialModule } from './modules/material-module';
 
 export const appConfig: ApplicationConfig = {
   // providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"retail-957d3","appId":"1:591224153669:web:927fa08db0fc893573e52c","storageBucket":"retail-957d3.appspot.com","apiKey":"AIzaSyC3-_3GKDhhxY43dKM-893IuoZY1MdQ8Vs","authDomain":"retail-957d3.firebaseapp.com","messagingSenderId":"591224153669"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
-  providers: [provideRouter(routes),
+  providers: [
+    provideRouter(routes),
     importProvidersFrom(AngularMaterialModule),
-      provideFirebaseApp(() =>
-      initializeApp(
-        environment.firebase
-      )),
-      provideAuth(() => getAuth()),
-      provideFirestore(() => getFirestore()),
-  ]
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    firebase: {
-        apiKey: "AIzaSyC3-_3GKDhhxY43dKM-893IuoZY1MdQ8Vs",
-        authDomain: "retail-957d3.firebaseapp.com",
-        projectId: "retail-957d3",
-        storageBucket: "retail-957d3.appspot.com",
-        messagingSenderId: "591224153669",
-        appId: "1:591224153669:web:927fa08db0fc893573e52c"  
-    },
+  firebase: {
+    apiKey: 'AIzaSyC3-_3GKDhhxY43dKM-893IuoZY1MdQ8Vs',
+    authDomain: 'retail-957d3.firebaseapp.com',
+    projectId: 'retail-957d3',
+    storageBucket: 'retail-957d3.appspot.com',
+    messagingSenderId: '591224153669',
+    appId: '1:591224153669:web:927fa08db0fc893573e52c',
+  },
 };
