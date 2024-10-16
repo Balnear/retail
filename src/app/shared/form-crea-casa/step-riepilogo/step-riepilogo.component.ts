78b67139d9f81bfdedcc5031756052c7b9a2f53d
Balnear <antoniomelchionda70@gmail.com>
2024-10-16 22:25:00 +0200

    riepilogo + fix-code

--- src/app/shared/form-crea-casa/step-riepilogo/step-riepilogo.component.ts
+++ src/app/shared/form-crea-casa/step-riepilogo/step-riepilogo.component.ts
@@ -1,12 +1,75 @@
+import { CommonModule, KeyValuePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  ControlContainer,
+  FormGroupDirective,
+} from '@angular/forms';
 
+import { AngularMaterialModule } from '../../../modules/material-module';
+import {
+  ICON_CONSTANT,
+  INPUT_CONSTANT,
+  LABEL_CONSTANT,
+} from '../../../constants';
+import { CaseService } from '../../../services/case.service';
+import { GenericStepperModal } from '../../generics';
+
+/** Componente per lo step di riepilogo */
 @Component({
   selector: 'app-step-riepilogo',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularMaterialModule,
+  ],
   templateUrl: './step-riepilogo.component.html',
-  styleUrl: './step-riepilogo.component.scss'
+  styleUrls: ['./step-riepilogo.component.scss'],
+  providers: [
+    KeyValuePipe,
+    {
+      provide: ControlContainer,
+      useExisting: FormGroupDirective,
+    },
+  ],
 })
 export class StepRiepilogoComponent {
+  /** Constante per le label degli input */
+  inputConstant = INPUT_CONSTANT;
+  /** Costante per le label generiche */
+  labelConstant = LABEL_CONSTANT;
+  /** Costante per le label delle icone */
+  iconConstant = ICON_CONSTANT;
+  /** Riferimento al valore del form */
+  formValue!: any;
 
+  /**
+   * Il costruttore della classe
+   * @param {CaseService} caseService L'injectable del service caseService
+   * @param {GenericStepperModal} genericStepperModal Accesso alla componente dello stepper
+   * @param {FormGroupDirecrive} parentF Direttiva di accesso al form contenente nel padre
+   */
+  constructor(
+    public caseService: CaseService,
+    private genericStepperModal: GenericStepperModal,
+    private parentF: FormGroupDirective
+  ) {
+    this.formValue = parentF.form.value;
+  }
+
+  /** Lifecyclehook dell'onInit */
+  ngOnInit() {
+    console.log(this.formValue, 'formvalue');
+  }
+
+  /**
+   * Cambio di step per la modifica delle informazioni
+   * @param {number} step lo step a cui rimandare
+   */
+  changeStep(step: number) {
+    this.genericStepperModal.changeStep(step);
+  }
 }
